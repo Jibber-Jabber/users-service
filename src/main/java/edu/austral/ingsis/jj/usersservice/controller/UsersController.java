7780b4c6f940d51065578dc49b18ea22c2060459
AgustindelaVega <agustin.delavega@ing.austral.edu.ar>
2021-04-30 20:29:54 -0300

    initial commit

---
+++ src/main/java/edu/austral/ingsis/jj/usersservice/controller/UsersController.java
@@ -0,0 +1,26 @@
+package edu.austral.ingsis.jj.usersservice.controller;
+
+import edu.austral.ingsis.jj.usersservice.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersService usersService){
+        this.usersService = usersService;
+    }
+
+    @GetMapping
+    public List<String> getAllUsers(){
+        return usersService.getAllUsers();
+    }
+}
